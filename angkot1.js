7a7141c0646f18cc6d6c7af18569d0a03385f29f
ahmadyastasyir <ahmadyastasyir2000@gmail.com>
2022-12-22 23:52:42 +0700

    latihan else if

--- angkot1.js
+++ angkot1.js
@@ -4,6 +4,8 @@ var noAngkot = 1;
 for (noAngkot = noAngkot; noAngkot <= jumlahAngkot; noAngkot++) {
   if (noAngkot <= angkotBeroperasi) {
     console.log("Angkot No. " + noAngkot + " Beroperasi dengan baik.");
+  } else if (noAngkot === 8) {
+    console.log("Angkot No. " + noAngkot + " Sedang lembur.");
   } else {
     console.log("Angkot No. " + noAngkot + " Sedang tidak beroperasi.");
   }
