0654d07ad52e7246b6ad8ae8e2ba4b8934fc8581
ahmadyastasyir <ahmadyastasyir2000@gmail.com>
2022-12-22 19:18:55 +0700

    update

---
+++ angkot1.js
@@ -0,0 +1,6 @@
+/*LATIHAN While javascript */
+var pola = 1;
+while (pola <= 10) {
+  console.log("Angkot No. " + pola + "beroperasi dengan baik.");
+  pola++;
+}
--- script.js
+++
@@ -1,5 +0,0 @@
-var a = "5",
-  b = "25",
-  c = "125";
-console.log("Hello Jasasqript");
-console.log("isi variabel a,b,c berturut turut adalah" + a, b, c);
