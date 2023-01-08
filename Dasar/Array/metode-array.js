e1b5271783e6fd0de38c559475530b46b429a55d
ahmadyastasyir <ahmadyastasyir2000@gmail.com>
2023-01-09 03:24:51 +0700

    .

---
+++ Dasar/Array/metode-array.js
@@ -0,0 +1,26 @@
+// Metode dalam array
+var arr = ["Sandhika", "Galih", "Nova"];
+
+// 1. join
+// console.log(arr.join(" - "));
+
+// 2. push & pop
+// arr.push('Doody','Fitri')
+// arr.pop();
+// arr.pop();
+
+// 3. unshift dan shift
+// arr.unshift('Doody');
+// arr.shift();
+// console.log(arr.join(" - "));
+
+// 4. splice
+// splice(indexAwal, mauDihapusBerapa-opt,
+// elemenBaru1, elemenBaru2, ...)
+// arr.splice(2, 0, 'Doody', 'Fitri');
+// console.log(arr.join(" - "));
+
+// 5. slice
+// slice(awal, akhir);
+// var arr2 = arr.slice(1,3);
+// console.log(arr1.join(" - "));
